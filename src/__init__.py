a453387eee6b21b7950816fc47ca67925ba1a5c4
D.Sanaja <dritsanaja@gmail.com>
2025-06-25 19:51:44 +0200

    final

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-# Virtual Camera Package
-__version__ = "1.0.0"
-__author__ = "VirtualCamera Team"
-
-from .core import *
-from .filters import *
-from .detection import *
-from .segmentation import *
